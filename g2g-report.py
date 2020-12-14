2f947e6595b696244eb0a64b30a701495349e712
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-12-14 15:09:17 +0100

    Refactor using Graphql

--- g2g-report.py
+++ g2g-report.py
@@ -23,13 +23,13 @@ import os
 import argparse
 import sys
 import json
+import requests
 
-from cli.gitlab import GitlabCli
 from jinja2 import Template
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--dist', action='store', required=True,
+parser.add_argument('--dist', action='store', required=False, default=None,
                     nargs='+', help='Dist to process')
 parser.add_argument('--components', action='store', required=False,
                     nargs='+', help='Components to process')
@@ -38,6 +38,79 @@ parser.add_argument('--gitlab', action='store', required=False,
                     default="https://gitlab.com")
 parser.add_argument('--verbose', action='store_true')
 parser.add_argument('--debug', action='store_true')
+
+query = """
+query {
+  group(fullPath: "QubesOS") {
+    projects {
+      nodes {
+        name
+        release40: pipelines(ref: "release4.0") {
+          nodes {
+            id
+            jobs {
+              nodes {
+                name
+                detailedStatus {
+                  detailsPath
+                  text
+                }
+              }
+            }
+            status
+          }
+        }
+        master: pipelines(ref: "master") {
+          nodes {
+            id
+            jobs {
+              nodes {
+                name
+                detailedStatus {
+                  detailsPath
+                  text
+                }
+              }
+            }
+            status
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+def query_pipelines(token):
+    r = requests.post('https://gitlab.com/api/graphql',
+                      headers={
+                          "Authorization": "Bearer " + token,
+                          "Content-Type": "application/json"
+                      },
+                      json={"query": query})
+    if not r.ok:
+        print(r.text)
+        return
+    raw_data = r.json()
+    if 'errors' in raw_data:
+        print(raw_data['errors'])
+        return
+    projects = raw_data['data']['group']['projects']['nodes']
+    data = {}
+    for proj in projects:
+        name = proj['name'][6:]
+        data[name] = {}
+        pipeline_40 = proj['release40']['nodes']
+        if pipeline_40:
+            job_40 = pipeline_40[0]['jobs']['nodes']
+            data[name]['4.0'] = job_40
+
+        pipeline_41 = proj['master']['nodes']
+        if pipeline_41:
+            jobs_41 = pipeline_41[0]['jobs']['nodes']
+            data[name]['4.1'] = jobs_41
+    return data
 
 
 def pipeline_status(status):
@@ -48,82 +121,76 @@ def pipeline_status(status):
     if status in ('failed', 'canceled', 'skipped'):
         return 'failure'
 
-    if status == 'success':
-        return status
+    if status in ('success', 'passed'):
+        return 'success'
 
 
 def main(args=None):
     args = parser.parse_args(args)
-    token = os.environ.get('GITLAB_API_TOKEN', None)
-    cli = GitlabCli(args.gitlab, token)
+    token = None
+    if os.environ.get('GITLAB_API_TOKEN', None):
+        token = os.environ['GITLAB_API_TOKEN']
+    elif os.path.exists(os.path.expanduser('~/.gitlab-token')):
+        with open(os.path.expanduser('~/.gitlab-token')) as f:
+            token = f.read().strip()
+
+    data = query_pipelines(token)
 
     # WIP: use distfile.json on Qubes repo
     with open('distfile.json') as fd:
         distfile_data = json.loads(fd.read())
 
-    # Get pipelines first
-    for component in distfile_data["components"].keys():
-        if args.components and component not in args.components:
-            continue
-        print("-> Fetching pipelines for %s..." % component)
-        for release in distfile_data["components"][component][
-                "releases"].keys():
-            branch = distfile_data["components"][component]["releases"][
-                release].get("branch", None)
-            distfile_data["components"][component]["releases"][release][
-                "pipeline"] = \
-                cli.get_pipeline('QubesOS', 'qubes-' + component, branch,
-                                 only_finished=True)
+    if args.dist:
+        dists = args.dist
+    else:
+        dists = []
+        for release in distfile_data["releases"].keys():
+            dists += ["dom0-%s" % dist for dist in distfile_data["releases"][release]["dom0"]]
+            dists += ["vm-%s" % dist for dist in distfile_data["releases"][release]["vm"]]
 
+    dists = sorted(set(dists))
     qubes_status = {}
-    for dist in args.dist:
+    for dist in dists:
         print("-> Generating report for %s..." % dist)
         qubes_status[dist] = {}
         for component in sorted(distfile_data["components"].keys()):
-            if args.components and component not in args.components:
-                continue
+            # if args.components and component not in args.components:
+            #     continue
             qubes_status[dist][component] = {}
-            for release in distfile_data["components"][component][
-                    "releases"].keys():
-                branch = distfile_data["components"][component]["releases"][
-                    release].get("branch", None)
+            component_data = distfile_data["components"][component]["releases"]
+            for release in component_data.keys():
+                branch = component_data[release].get("branch", None)
                 qubes_status[dist][component][release] = {}
                 if branch:
-                    pipeline = \
-                        distfile_data["components"][component]["releases"][
-                            release]["pipeline"]
+                    pipeline = data.get(component, {}).get(release, [])
                     if pipeline:
                         build_job = None
                         install_job = None
                         repro_job = None
-                        for job in pipeline.jobs.list():
-                            if job.name == "build:%s" % dist:
+                        for job in pipeline:
+                            if job['name'] == "build:%s" % dist:
                                 build_job = job
-                            elif job.name == "install:%s" % dist:
+                            elif job['name'] == "install:%s" % dist:
                                 install_job = job
-                            elif job.name == "repro:%s" % dist:
+                            elif job['name'] == "repro:%s" % dist:
                                 repro_job = job
 
                         if build_job:
                             qubes_status[dist][component][release]["build"] = {
-                                "url": build_job.web_url,
-                                "badge": "build_%s.svg" % pipeline_status(
-                                    build_job.status),
+                                "url": args.gitlab + build_job['detailedStatus']['detailsPath'],
+                                "badge": "build_%s.svg" % pipeline_status(build_job['detailedStatus']['text']),
                                 "text": "Build Status"
                             }
                         if install_job:
-                            qubes_status[dist][component][release][
-                                "install"] = {
-                                "url": install_job.web_url,
-                                "badge": "install_%s.svg" % pipeline_status(
-                                    install_job.status),
+                            qubes_status[dist][component][release]["install"] = {
+                                "url": args.gitlab + install_job['detailedStatus']['detailsPath'],
+                                "badge": "install_%s.svg" % pipeline_status(install_job['detailedStatus']['text']),
                                 "text": "Install Status"
                             }
                         if repro_job:
                             qubes_status[dist][component][release]["repro"] = {
-                                "url": repro_job.web_url,
-                                "badge": "repro_%s.svg" % pipeline_status(
-                                    repro_job.status),
+                                "url": args.gitlab + repro_job['detailedStatus']['detailsPath'],
+                                "badge": "repro_%s.svg" % pipeline_status(repro_job['detailedStatus']['text']),
                                 "text": "Repro Status"
                             }
 
