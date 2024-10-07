5c9753c8ad05369d87d8d85d9d369b75a00910f7
Guillaume Chinal <guillaume.chinal@gmail.com>
2024-10-07 20:54:06 +0200

    switching to object

--- g2g-report.py
+++ g2g-report.py
@@ -27,210 +27,173 @@ import requests
 
 from jinja2 import Template
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument('--dist', action='store', required=False, default=None,
-                    nargs='+', help='Dist to process')
-parser.add_argument('--components', action='store', required=False,
-                    nargs='+', help='Components to process')
-parser.add_argument('--gitlab', action='store', required=False,
-                    help='Gitlab instance URL',
-                    default="https://gitlab.com")
-parser.add_argument('--verbose', action='store_true')
-parser.add_argument('--debug', action='store_true')
+class ReportBuilder:
+    def __init__(self, gitlab_url: str, current_release: str, next_release: str):
+        self._current_release = current_release
+        self._gitlab_url = gitlab_url
+        self._next_release = next_release
 
-query = """
-query {
-  group(fullPath: "QubesOS") {
-    projects {
-      nodes {
-        name
-        release41: pipelines(ref: "release4.1", first: 1) {
-          nodes {
-            id
-            jobs {
-              nodes {
-                name
-                detailedStatus {
-                  detailsPath
-                  text
-                }
-              }
-            }
-            status
-          }
+    def error_and_exit(self, error_message: str):
+        """Print an error message and exit program"""
+
+        print(error_message, file=sys.stderr)
+        raise RuntimeError
+
+
+    def get_pipelines_data(self, token: str):
+        projects = query_pipelines(token)['data']['group']['projects']['nodes']
+        data = {}
+        for proj in projects:
+            name = proj['name'][6:]
+            data[name] = {}
+            pipeline_41 = proj['release41']['nodes']
+            if pipeline_41:
+                job_41 = pipeline_41[0]['jobs']['nodes']
+                data[name]['4.1'] = job_41
+
+            pipeline_42 = proj['main']['nodes']
+            if pipeline_42:
+                jobs_42 = pipeline_42[0]['jobs']['nodes']
+                data[name]['4.2'] = jobs_42
+        return data
+
+
+    def query_pipelines(token: str):
+        headers = {
+            "Content-Type": "application/json",
         }
-        main: pipelines(ref: "main", first: 1) {
-          nodes {
-            id
-            jobs {
-              nodes {
-                name
-                detailedStatus {
-                  detailsPath
-                  text
-                }
-              }
-            }
-            status
-          }
-        }
-      }
-    }
-  }
-}
-"""
+
+        if token:
+            headers["Authorization"] = f"Bearer {token}"
+
+        r = requests.post('https://gitlab.com/api/graphql',
+                        headers=headers,
+                        json={"query": query})
+        if not r.ok:
+            error_and_exit(r.text)
+
+        raw_data = r.json()
+
+        if 'errors' in raw_data:
+            error_and_exit(raw_data['errors'])
+
+        return raw_data
 
 
-def error_and_exit(error_message: str):
-    """Print an error message and exit program"""
+    def pipeline_status(status):
+        if status in ('created', 'waiting_for_resource', 'preparing', 'pending',
+                    'running', 'manual', 'scheduled'):
+            return 'unknown'
 
-    print(error_message, file=sys.stderr)
-    raise RuntimeError
+        if status in ('failed', 'canceled', 'skipped'):
+            return 'failure'
 
-
-def get_pipelines_data(token: str):
-    projects = query_pipelines(token)['data']['group']['projects']['nodes']
-    data = {}
-    for proj in projects:
-        name = proj['name'][6:]
-        data[name] = {}
-        pipeline_41 = proj['release41']['nodes']
-        if pipeline_41:
-            job_41 = pipeline_41[0]['jobs']['nodes']
-            data[name]['4.1'] = job_41
-
-        pipeline_42 = proj['main']['nodes']
-        if pipeline_42:
-            jobs_42 = pipeline_42[0]['jobs']['nodes']
-            data[name]['4.2'] = jobs_42
-    return data
+        if status in ('success', 'passed'):
+            return 'success'
 
 
-def query_pipelines(token: str):
-    headers = {
-        "Content-Type": "application/json",
-    }
+    def generate_report(args=None):
+        parser = argparse.ArgumentParser()
 
-    if token:
-        headers["Authorization"] = f"Bearer {token}"
+        parser.add_argument('--dist', action='store', required=False, default=None,
+                            nargs='+', help='Dist to process')
+        parser.add_argument('--components', action='store', required=False,
+                            nargs='+', help='Components to process')
+        parser.add_argument('--gitlab', action='store', required=False,
+                            help='Gitlab instance URL',
+                            default="https://gitlab.com")
+        parser.add_argument('--verbose', action='store_true')
+        parser.add_argument('--debug', action='store_true')
+        args = parser.parse_args(args)
+        token = None
+        if os.environ.get('GITLAB_API_TOKEN', None):
+            token = os.environ['GITLAB_API_TOKEN']
+        elif os.path.exists(os.path.expanduser('~/.gitlab-token')):
+            with open(os.path.expanduser('~/.gitlab-token')) as f:
+                token = f.read().strip()
 
-    r = requests.post('https://gitlab.com/api/graphql',
-                      headers=headers,
-                      json={"query": query})
-    if not r.ok:
-        error_and_exit(r.text)
+        data = get_pipelines_data(token)
 
-    raw_data = r.json()
+        # WIP: use distfile.json on Qubes repo
+        with open('distfile.json') as fd:
+            distfile_data = json.loads(fd.read())
 
-    if 'errors' in raw_data:
-        error_and_exit(raw_data['errors'])
+        if args.dist:
+            dists = args.dist
+        else:
+            dists = []
+            for release in distfile_data["releases"].keys():
+                dists += ["dom0-%s" % dist for dist in distfile_data["releases"][release]["dom0"]]
+                dists += ["vm-%s" % dist for dist in distfile_data["releases"][release]["vm"]]
 
-    return raw_data
+        dists = sorted(set(dists))
+        qubes_status = {}
+        for dist in dists:
+            print("-> Generating report for %s..." % dist)
+            qubes_status[dist] = {}
+            for component in sorted(distfile_data["components"].keys()):
+                # if args.components and component not in args.components:
+                #     continue
+                qubes_status[dist][component] = {}
+                component_data = distfile_data["components"][component]["releases"]
+                for release in component_data.keys():
+                    branch = component_data[release].get("branch", None)
+                    qubes_status[dist][component][release] = {}
+                    if branch:
+                        pipeline = data.get(component, {}).get(release, [])
+                        if pipeline:
+                            build_job = None
+                            install_job = None
+                            repro_job = None
+                            for job in pipeline:
+                                if job['name'] == "build:%s" % dist:
+                                    build_job = job
+                                elif job['name'] == "install:%s" % dist:
+                                    install_job = job
+                                elif job['name'] == "repro:%s" % dist:
+                                    repro_job = job
 
+                            if build_job:
+                                qubes_status[dist][component][release]["build"] = {
+                                    "url": args.gitlab + build_job['detailedStatus']['detailsPath'],
+                                    "badge": "build_%s.svg" % pipeline_status(build_job['detailedStatus']['text']),
+                                    "text": "Build Status"
+                                }
+                            if install_job:
+                                qubes_status[dist][component][release]["install"] = {
+                                    "url": args.gitlab + install_job['detailedStatus']['detailsPath'],
+                                    "badge": "install_%s.svg" % pipeline_status(install_job['detailedStatus']['text']),
+                                    "text": "Install Status"
+                                }
+                            if repro_job:
+                                qubes_status[dist][component][release]["repro"] = {
+                                    "url": args.gitlab + repro_job['detailedStatus']['detailsPath'],
+                                    "badge": "repro_%s.svg" % pipeline_status(repro_job['detailedStatus']['text']),
+                                    "text": "Repro Status"
+                                }
 
-def pipeline_status(status):
-    if status in ('created', 'waiting_for_resource', 'preparing', 'pending',
-                  'running', 'manual', 'scheduled'):
-        return 'unknown'
-
-    if status in ('failed', 'canceled', 'skipped'):
-        return 'failure'
-
-    if status in ('success', 'passed'):
-        return 'success'
-
-
-def main(args=None):
-    args = parser.parse_args(args)
-    token = None
-    if os.environ.get('GITLAB_API_TOKEN', None):
-        token = os.environ['GITLAB_API_TOKEN']
-    elif os.path.exists(os.path.expanduser('~/.gitlab-token')):
-        with open(os.path.expanduser('~/.gitlab-token')) as f:
-            token = f.read().strip()
-
-    data = get_pipelines_data(token)
-
-    # WIP: use distfile.json on Qubes repo
-    with open('distfile.json') as fd:
-        distfile_data = json.loads(fd.read())
-
-    if args.dist:
-        dists = args.dist
-    else:
-        dists = []
-        for release in distfile_data["releases"].keys():
-            dists += ["dom0-%s" % dist for dist in distfile_data["releases"][release]["dom0"]]
-            dists += ["vm-%s" % dist for dist in distfile_data["releases"][release]["vm"]]
-
-    dists = sorted(set(dists))
-    qubes_status = {}
-    for dist in dists:
-        print("-> Generating report for %s..." % dist)
-        qubes_status[dist] = {}
-        for component in sorted(distfile_data["components"].keys()):
-            # if args.components and component not in args.components:
-            #     continue
-            qubes_status[dist][component] = {}
-            component_data = distfile_data["components"][component]["releases"]
-            for release in component_data.keys():
-                branch = component_data[release].get("branch", None)
-                qubes_status[dist][component][release] = {}
-                if branch:
-                    pipeline = data.get(component, {}).get(release, [])
-                    if pipeline:
-                        build_job = None
-                        install_job = None
-                        repro_job = None
-                        for job in pipeline:
-                            if job['name'] == "build:%s" % dist:
-                                build_job = job
-                            elif job['name'] == "install:%s" % dist:
-                                install_job = job
-                            elif job['name'] == "repro:%s" % dist:
-                                repro_job = job
-
-                        if build_job:
-                            qubes_status[dist][component][release]["build"] = {
-                                "url": args.gitlab + build_job['detailedStatus']['detailsPath'],
-                                "badge": "build_%s.svg" % pipeline_status(build_job['detailedStatus']['text']),
-                                "text": "Build Status"
-                            }
-                        if install_job:
-                            qubes_status[dist][component][release]["install"] = {
-                                "url": args.gitlab + install_job['detailedStatus']['detailsPath'],
-                                "badge": "install_%s.svg" % pipeline_status(install_job['detailedStatus']['text']),
-                                "text": "Install Status"
-                            }
-                        if repro_job:
-                            qubes_status[dist][component][release]["repro"] = {
-                                "url": args.gitlab + repro_job['detailedStatus']['detailsPath'],
-                                "badge": "repro_%s.svg" % pipeline_status(repro_job['detailedStatus']['text']),
-                                "text": "Repro Status"
-                            }
+                    # no need to display empty lines?
+                    if not qubes_status[dist][component][release]:
+                        del qubes_status[dist][component][release]
 
                 # no need to display empty lines?
-                if not qubes_status[dist][component][release]:
-                    del qubes_status[dist][component][release]
+                if not qubes_status[dist][component]:
+                    del qubes_status[dist][component]
 
-            # no need to display empty lines?
-            if not qubes_status[dist][component]:
-                del qubes_status[dist][component]
+        with open('template.md.jinja', 'r') as template_fd:
+            template_md = Template(template_fd.read())
 
-    with open('template.md.jinja', 'r') as template_fd:
-        template_md = Template(template_fd.read())
+        with open('template.html.jinja', 'r') as template_fd:
+            template_html = Template(template_fd.read())
 
-    with open('template.html.jinja', 'r') as template_fd:
-        template_html = Template(template_fd.read())
+        data = {"qubes_status": qubes_status}
 
-    data = {"qubes_status": qubes_status}
+        with open('public/index.md', 'w') as fd:
+            fd.write(template_md.render(**data))
 
-    with open('public/index.md', 'w') as fd:
-        fd.write(template_md.render(**data))
-
-    with open('public/index.html', 'w') as fd:
-        fd.write(template_html.render(**data))
+        with open('public/index.html', 'w') as fd:
+            fd.write(template_html.render(**data))
 
 
 if __name__ == '__main__':
